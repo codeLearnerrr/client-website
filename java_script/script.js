1a56765ed425a8ae8c844f8f20210c7df6407323
Jeferson Eiji <jeferson.eiji@icloud.com>
2020-08-18 22:42:24 -0300

    implement cards in services section

---
+++ java_script/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        let position = $(this).scrollTop();
+        if (position >= 540) {
+            $('.nav-menu').addClass('custom-navbar');
+            $('.text-light').addClass('text-dark');
+            $('.text-white').addClass('text-dark');
+        } else {
+            $('.nav-menu').removeClass('custom-navbar');
+            $('.text-light').removeClass('text-dark');
+            $('.text-white').removeClass('text-dark');
+       }
+    });
+});
