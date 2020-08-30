4c23a58e76e6ce6e7a2b3174a6f24e90cd5562a6
Jeferson Eiji <jeferson.eiji@icloud.com>
2020-08-29 23:31:48 -0300

    keep refactoring scss and html

--- java_script/script.js
+++ java_script/script.js
@@ -1,15 +1,63 @@
 $(document).ready(function () {
+    $('.nav-button').click(function () {
+        $('.nav-button').toggleClass('change')
+    });
     $(window).scroll(function () {
         let position = $(this).scrollTop();
         if (position >= 540) {
             $('.nav-menu').addClass('custom-navbar');
-            $('.text-light').addClass('text-dark');
-            $('.text-white').addClass('text-dark');
+
         } else {
             $('.nav-menu').removeClass('custom-navbar');
-            $('.text-light').removeClass('text-dark');
-            $('.text-white').removeClass('text-dark');
-       }
+
+        }
     });
 });
 
+// $(document).ready(function () {
+//     $('.nav-button').click(function () {
+//         $('.nav-button').toggleClass('change')
+//     });
+//     $(window).scroll(function () {
+//         let position = $(this).scrollTop();
+//         if (position >= 200) {
+//             $('.nav-menu').addClass('custom-navbar');
+//         } else {
+//             $('.nav-menu').removeClass('custom-navbar');
+//         }
+//     });
+//     $(window).scroll(function () {
+//         let position = $(this).scrollTop();
+//         if (position >= 650) {
+//             $('.camera-img').addClass('fromLeft');
+//             $('.mission-text').addClass('fromRight');
+//         } else {
+//             $('.camera-img').addClass('fromLeft');
+//             $('.mission-text').addClass('fromRight');
+//         }
+//     });
+//     $('.gallery-list-item').click(function () {
+//         let value = $(this).attr('data-filter');
+//         if (value === 'all') {
+//             $('.filter').show(300);
+//         } else {
+//             $('.filter').not('.' + value).hide(300);
+//             $('.filter').filter('.' + value).show(300);
+//         }
+//     });
+//     $('.gallery-list-item').click(function () {
+//         $(this).addClass('active-item ').siblings().removeClass('active-item');
+//     });
+//     $(window).scroll(function () {
+//         let position = $(this).scrollTop();
+//         if (position >= 4300) {
+//             $('.card-1').addClass('moveFromLeft');
+//             $('.card-2').addClass('moveFromBottom');
+//             $('.card-3').addClass('moveFromRight');
+//         } else {
+//             $('.card-1').removeClass('moveFromLeft');
+//             $('.card-2').removeClass('moveFromBottom');
+//             $('.card-3').removeClass('moveFromRight');
+//         }
+//     });
+// });
