e1310c2cdbba30b0e5b616e34024bf0ebd67c3de
Jeferson Eiji <jeferson.eiji@icloud.com>
2020-09-13 21:39:58 -0300

    fix maps inline event and content

--- java_script/script.js
+++ java_script/script.js
@@ -15,15 +15,17 @@ $(document).ready(function () {
         }
     });
 });
+window.addEventListener('load', initMap())
 
-// Initialize and add the map
 function initMap() {
     // The office location
-    var office = { lat: -19.919054, lng: -43.938624 };
+    var office = { lat: -19.943346 , lng: -44.037707 };
     // The map, centered at office location
     var map = new google.maps.Map(
         document.getElementById('map'), { zoom: 18, center: office });
     // The marker, positioned at office
-    var marker = new google.maps.Marker({ position: office, map: map });
+   var marker = new google.maps.Marker({ position: office, map: map });
 }
 
+
+ 
