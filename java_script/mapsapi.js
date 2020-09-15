c40ba3c76fe4554e72a6ceafac3b317f7859c058
Jeferson Eiji <jeferson.eiji@icloud.com>
2020-09-14 21:24:35 -0300

    finished minification

--- java_script/mapsapi.js
+++ java_script/mapsapi.js
@@ -1,11 +1,1 @@
-let map;
-
-function initMap() {
-    // The office location
-    var office = { lat: -19.943346 , lng: -44.037707 };
-    // The map, centered at office location
-    var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 18, center: office });
-    // The marker, positioned at office
-   var marker = new google.maps.Marker({ position: office, map: map });
-}
+var map;function initMap(){var a={lat:-19.943346,lng:-44.037707},b=new google.maps.Map(document.getElementById("map"),{zoom:18,center:a});new google.maps.Marker({position:a,map:b})};
